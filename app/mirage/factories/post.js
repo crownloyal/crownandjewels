067e0753a649674d26d8b7b696cf97df6effa2ff
AEROdom <dom@aeromovement.com>
2016-03-30 23:55:37 +0100

    templating
    - main template changes

---
+++ app/mirage/factories/post.js
@@ -0,0 +1,17 @@
+/*
+  This is an example factory definition.
+
+  Create more files in this directory to define additional factories.
+*/
+// import Mirage/*, {faker} */ from 'ember-cli-mirage';
+
+// export default Mirage.Factory.extend({
+//   name: 'Jewels',                         // strings
+//   age: 22,                              // numbers
+  
+//   email: 'crownandjewelsblog@gmail.com'
+
+//   // firstName: faker.name.firstName,       // using faker
+//   // lastName: faker.name.firstName,
+//   // zipCode: faker.address.zipCode
+// });
--- app/routes/post.js
+++ app/routes/post.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model(params) {
-		return store.findRecord('post', params.id);
+		return this.store.findRecord('post', params.id);
 	}
 });
